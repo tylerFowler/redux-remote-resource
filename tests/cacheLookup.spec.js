94702011cbadf2c78768370a74a8dbb65c36586a
Tyler Fowler <tylerfowler.1337@gmail.com>
2017-02-04 23:23:55 -0600

    Add tests for promised based cache mapping fns

--- tests/cacheLookup.spec.js
+++ tests/cacheLookup.spec.js
@@ -5,8 +5,8 @@ const { cacheLookup } = require('../lib/requestBuilders');
 
 const cacheMapMock = () => true;
 
-test('cacheLookup', t => {
-  t.plan(6);
+test.only('cacheLookup', t => {
+  t.plan(8);
 
   cacheLookup({}, { method: 'POST', nocache: false, cacheMapping: cacheMapMock })
   .then(res => t.notok(res, 'skips cache for uncacheable methods'))
@@ -26,6 +26,16 @@ test('cacheLookup', t => {
 
   cacheLookup({ k: 'value' }, { method: 'GET', cacheMapping: state => state.k })
   .then(res => t.ok(res, 'state is passed to cacheMapping'))
+  .catch(t.fail);
+
+  const promiseMapping = () => Promise.resolve(2);
+  cacheLookup({}, { method: 'GET', cacheMapping: promiseMapping })
+  .then(res => t.equal(res, 2, 'promises are allowed in mapping fn'))
+  .catch(t.fail);
+
+  const multiPromiseMapping = state => Promise.resolve().then(() => state.v);
+  cacheLookup({ v: 2 }, { method: 'GET', cacheMapping: multiPromiseMapping })
+  .then(res => t.equal(res, 2, 'promises are evaluated to completion'))
   .catch(t.fail);
 
   const errCacheMapping = () => { throw new Error('Error!'); };
