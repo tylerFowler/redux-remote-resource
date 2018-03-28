d81a31a039c4e2009c58ca899cd856fe4ad6a844
Tyler Fowler <tylerfowler.1337@gmail.com>
2018-03-27 19:43:07 -0500

    Actually use `bypassStatusActions` option 🙄

--- tests/makeStatusActions.spec.js
+++ tests/makeStatusActions.spec.js
@@ -1,16 +1,17 @@
 require('babel-register');
 
-const test              = require('tape');
-const makeStatusActions = require('../lib/builders/makeStatusActions').default;
+const test                = require('tape');
+const makeStatusActions   = require('../lib/builders/makeStatusActions').default;
+const { defaultCallOpts } = require('../lib/middleware');
 
-test('makeStatusActions', t => {
-  t.plan(8);
+test.only('makeStatusActions', t => {
+  t.plan(9);
 
   const plainActions = { 400: 'SOME_ACTION' };
   const plainDispatch = action => t.equal(
     action.type, 'SOME_ACTION', 'dispatches given action as type'
   );
-  makeStatusActions(plainActions, plainDispatch)
+  makeStatusActions(plainActions, defaultCallOpts, plainDispatch)
     .then(sa => sa[400]())
     .catch(t.fail);
 
@@ -22,13 +23,13 @@ test('makeStatusActions', t => {
       t.equal(res, fnRes, 'called action w/ response');
     }
   };
-  makeStatusActions(fnActions, fnDispatch)
+  makeStatusActions(fnActions, defaultCallOpts, fnDispatch)
     .then(sa => sa[400](fnRes))
     .catch(t.fail);
 
   const objActions = { 400: { type: 'FORBIDDEN_FRUIT' } };
   const objDispatch = act => t.equal(act, objActions[400], 'dispatches action');
-  makeStatusActions(objActions, objDispatch)
+  makeStatusActions(objActions, defaultCallOpts, objDispatch)
     .then(sa => sa[400]())
     .catch(t.fail);
 
@@ -36,7 +37,7 @@ test('makeStatusActions', t => {
   const pDispatch = act => {
     t.equal(act.type, 'BAD_ERROR', 'dispatches promised values');
   };
-  makeStatusActions(pActions, pDispatch)
+  makeStatusActions(pActions, defaultCallOpts, pDispatch)
     .then(sa => sa[400]())
     .catch(t.fail);
 
@@ -45,7 +46,7 @@ test('makeStatusActions', t => {
   };
   const rpDispatch = act =>
     t.equal(act.type, 'BAD_ERROR', 'dispatches promised values recursively');
-  makeStatusActions(rpActions, rpDispatch)
+  makeStatusActions(rpActions, defaultCallOpts, rpDispatch)
     .then(sa => sa[400]())
     .catch(t.fail);
 
@@ -56,17 +57,25 @@ test('makeStatusActions', t => {
     403: () => {}
   };
   const multDispatch = () => {};
-  makeStatusActions(multActions, multDispatch)
+  makeStatusActions(multActions, defaultCallOpts, multDispatch)
     .then(sa => {
       t.true(
         sa.hasOwnProperty(400) && sa.hasOwnProperty(401) &&
-      sa.hasOwnProperty(402) && sa.hasOwnProperty(403),
+        sa.hasOwnProperty(402) && sa.hasOwnProperty(403),
         'actions are reduced back into an object keyed by status codes'
       );
 
       t.true(
         Object.keys(sa).every(k => typeof sa[k] === 'function'),
         'actions values are reduced into callback functions'
+      );
+    }).catch(t.fail);
+
+  makeStatusActions(plainActions, { bypassStatusActions: true }, () => true)
+    .then(sa => {
+      t.deepEqual(
+        sa, {},
+        'returns no actions if bypassStatusActions is enabled'
       );
     }).catch(t.fail);
 });
