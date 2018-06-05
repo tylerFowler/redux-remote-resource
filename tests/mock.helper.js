13a637d13c881f2555a06eae4ffd0d5360f22329
Tyler Fowler <tylerfowler.1337@gmail.com>
2018-06-05 16:08:58 -0500

    Add fetch mock

--- tests/mock.helper.js
+++ tests/mock.helper.js
@@ -1,4 +1,51 @@
 const failTest = t => error => { t.fail(error); t.end(); };
 const nest = name => `  -> ${name}`;
 
-export { failTest, nest };
+function createFetchMock() {
+  let givenOptions = {};
+  let headers = {};
+  let method;
+  let body;
+  let responseStatus = 200;
+  let requestedUrl;
+
+  return {
+    fetchFunc: function fetchMock(url, opts) {
+      headers = opts.headers;
+      method = opts.method;
+      givenOptions = opts;
+      requestedUrl = url;
+
+      const response = {
+        status: responseStatus,
+        statusText: `Returning response of ${responseStatus}`,
+        ok: responseStatus < 400,
+        bodyData: body,
+        json() {
+          return new Promise((resolve, reject) => {
+            try {
+              const json = JSON.parse(body);
+              resolve(json);
+            } catch (error) {
+              reject(error);
+            }
+          });
+        }
+      };
+
+      return Promise.resolve(response);
+    },
+    respondWith(respStatus, respBody) {
+      responseStatus = respStatus;
+      body = JSON.stringify(respBody);
+    },
+    options() { return givenOptions; },
+    headers() { return headers; },
+    method() { return method; },
+    body() { return body; },
+    responseStatus() { return responseStatus; },
+    url() { return requestedUrl; }
+  };
+}
+
+export { failTest, nest, createFetchMock };
